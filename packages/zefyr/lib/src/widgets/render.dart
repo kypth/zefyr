b9e0f0f77c9620266c589f3fc48b801f4ac5d5fc
Kien Dau <knightvincv@gmail.com>
2018-11-04 18:22:26 +0700

    Init ZefyrRender

---
+++ packages/zefyr/lib/src/widgets/render.dart
@@ -0,0 +1,29 @@
+// Copyright (c) 2018, the Zefyr project authors.  Please see the AUTHORS file
+// for details. All rights reserved. Use of this source code is governed by a
+// BSD-style license that can be found in the LICENSE file.
+import 'dart:async';
+
+import 'package:collection/collection.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter/material.dart';
+import 'package:notus/notus.dart';
+
+import 'code.dart';
+import 'common.dart';
+import 'controller.dart';
+import 'editable_box.dart';
+import 'editor.dart';
+import 'image.dart';
+import 'input.dart';
+import 'list.dart';
+import 'paragraph.dart';
+import 'quote.dart';
+import 'render_context.dart';
+import 'selection.dart';
+
+class ZefyrRender extends StatelessWidget {
+  Widget build(BuildContext context) {
+    return Text('hello');
+  }
+}
